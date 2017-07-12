ea19027a0954bdf84f27abb3c05e9535717c94e3
Nick Sweeting <git@nicksweeting.com>
2017-07-12 01:43:22 -0500

    fix chat

--- js/serverless-webrtc.js
+++ js/serverless-webrtc.js
@@ -1,8 +1,18 @@
+console.log('Confirm that this sends no outside network requests in the Network Panel.')
+
+var receiveProgress = document.getElementById('receiveProgress')
+var sendProgress = document.getElementById('sendProgress')
+var bitrateDiv = document.getElementById('bitrate')
+var statusMessage = document.getElementById('status')
+var downloadsAnchor = document.getElementById('downloads')
+
 function WebRTCChat(cfg, con, sendTyping) {
     var self = this;
 
 /* WebRTC setup for broser-to-browser connection */
-    self.cfg = {'iceServers':[{"url":"stun:23.21.150.121"}]}; //{"url":"stun:23.21.150.121"}
+    self.cfg = {'iceServers':[{
+      'url': 'stun:stun.l.google.com:19302'
+    }]}; //{"url":"stun:23.21.150.121"}
     self.con = {'optional':  [{'DtlsSrtpKeyAgreement': true}] };
     self.activeChannel;
     self.activeConnection;
@@ -11,7 +21,7 @@ function WebRTCChat(cfg, con, sendTyping) {
     self.readystate = false;
     self.sendTyping = true;
 
-/* WebRTC + PGP CHAT CONNECTION CODE */
+/* WebRTC + CHAT CONNECTION CODE */
 
     /* THE HOST (initiates the chat) */
 
@@ -19,9 +29,12 @@ function WebRTCChat(cfg, con, sendTyping) {
         var hostConnection = new RTCPeerConnection(self.cfg, self.con);                                 // init connection
         self.initConnection(hostConnection, offer_callback);
 
-        var hostChannel = hostConnection.createDataChannel('chat', {reliable:true, ordered:true});      // init channel
+        var hostChannel = hostConnection.createDataChannel('chat', {
+            reliable: true,
+            ordered: true,
+        });
         self.initChannel(hostChannel, ready_callback);
-        
+
         console.log("Creating RTC Chat Host Offer...");
         hostConnection.createOffer(self.handleDescription, self.handleDescriptionFailure);
         // copy paste this offer to all clients who want to join
@@ -32,7 +45,7 @@ function WebRTCChat(cfg, con, sendTyping) {
         var answerDesc = new RTCSessionDescription(answer.rtc);
         console.log("Received Chat RTC Join Answer: ", answerDesc);
         self.activeConnection.setRemoteDescription(answerDesc);
-
+        self.activeChannel.onopen()
         writeToChatLog("Started hosting a chat.", "text-success alert-success");
 
         // hostChannel.onopen will trigger once the connection is complete (enabling the chat window)
@@ -44,10 +57,11 @@ function WebRTCChat(cfg, con, sendTyping) {
         var clientConnection = new RTCPeerConnection(cfg, con);
         self.initConnection(clientConnection, answer_callback);
 
-        clientConnection.ondatachannel = function (e) {                                     // once client receives a good data channel from the host
+        self.activeConnection.ondatachannel = function (e) {                                     // once client receives a good data channel from the host
             // Chrome sends event, FF sends raw channel
             var clientChannel = e.channel || e;
             self.initChannel(clientChannel, ready_callback);
+            self.activeChannel.onopen()
             writeToChatLog("Joined a chat.", "text-success alert-success");
             // clientChannel.onopen will then trigger once the connection is complete (enabling the chat window)
         };
@@ -57,7 +71,7 @@ function WebRTCChat(cfg, con, sendTyping) {
         var offerDesc = new RTCSessionDescription(offer.rtc);
         console.log("Received Chat RTC Host Offer: ", offerDesc);
         self.activeConnection.setRemoteDescription(offerDesc);
-        
+
         console.log("Answering Chat Host Offer...");
         self.activeConnection.createAnswer(self.handleDescription, self.handleDescriptionFailure);
 
@@ -67,15 +81,16 @@ function WebRTCChat(cfg, con, sendTyping) {
     self.initConnection = function(conn, callback) {
         self.activeConnection = conn;
         // these aren't really necessary
-        conn.addStream(window.myStream);
         conn.onconnection                   = function (state) {console.info('Chat connection complete: ', event);}
         conn.onsignalingstatechange         = function (state) {console.info('Signaling state change: ', state); if (self.activeConnection.iceConnectionState == "disconnected") self.writeToChatLog("Chat partner disconnected.", "text-warning alert-error");}
         conn.oniceconnectionstatechange     = function (state) {console.info('Signaling ICE connection state change: ', state); if (self.activeConnection.iceConnectionState == "disconnected") self.writeToChatLog("Chat partner disconnected.", "text-warning alert-error");}
         conn.onicegatheringstatechange      = function (state) {console.info('Signaling ICE setup state change: ', state);}
         conn.onaddstream = function (event) {
             var video = document.getElementById('theirVideo');
-            if (window.URL) video.src = window.URL.createObjectURL(event.stream);
-            else video.src = event.stream;
+            if (window.URL)
+                video.src = window.URL.createObjectURL(event.stream);
+            else
+                video.src = event.stream;
             console.log("YAAAAAAAAgotvideo");
         }
         //this is the important one
@@ -97,8 +112,13 @@ function WebRTCChat(cfg, con, sendTyping) {
 
     self.initChannel = function(chan, callback) {
         self.activeChannel = chan;
+        self.activeChannel.binaryType = 'arraybuffer';
         // once the channel is open, trigger the callback to enable the chat window or carry out other logic
-        chan.onopen = function (e) { console.log('Data Channel Connected.'); self.readystate = true; if (callback) callback(e);}
+        self.activeChannel.onopen = function (e) {
+            console.log('Data Channel Connected.');
+            self.readystate = true;
+            if (callback) callback(e);
+        }
         chan.onmessage = self.receiveMessage;
         console.log("Initialized Data Channel: ", chan);
     }
@@ -124,18 +144,156 @@ function WebRTCChat(cfg, con, sendTyping) {
     }
 
     self.receiveMessage = function(event) {
-        var data = JSON.parse(event.data);
-        if (data.type === 'file' || event.data.size) console.log("Receiving a file.");
-        else {
-            if (data.typing && !data.message) {
-                console.log("Partner is typing...");
-                self.displayPartnerTyping();
-            }
-            else {
-                console.log("Received a message: ", data.message);
-                self.writeToChatLog(data.message, "text-info recv", false);
-            }
+        try {
+            var data = JSON.parse(event.data);
+        } catch(e) {
+            console.log('RECEIVING FILE')
+            return self.receiveFile(event);
         }
+
+        if (data.file) {
+            return self.prepareToReceiveFile(data);
+        }
+
+        if (data.typing && !data.message) {
+            console.log("Partner is typing...");
+            self.displayPartnerTyping();
+        }
+        else {
+            console.log("Received a message: ", data.message);
+            self.writeToChatLog(data.message, "text-info recv", false);
+        }
+    }
+
+    // Audio and Video
+
+    self.initVideo = function() {
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+        var video = document.getElementById('myVideo');
+
+        navigator.getUserMedia({
+            audio: false,
+            video: true
+        }, function (stream) {
+          if (window.URL)
+            video.src = window.URL.createObjectURL(stream);
+          else
+            video.src = stream;
+          window.myStream = stream;
+          self.activeConnection.addStream(window.myStream);
+        }, function (error){
+          console.log('navigator.getUserMedia error: ', error);
+        });
+    }
+
+    // Files
+
+    self.prepareToReceiveFile = function(data) {
+        self.startTime = null;
+        self.receiveBuffer = null;
+        self.receivedSize = null;
+        self.bitrateMax = null;
+        self.numReceivedFiles = self.numReceivedFiles || 0;
+        self.receivedFiles = self.receivedFiles || {};
+        self.writeToChatLog(
+            "Receiving " + data.name + " (" + data.size + " bytes)",
+            "text-success sent insecure",
+            false
+        );
+        self.numReceivedFiles += 1;
+        // XSS out the wazoo
+        var downloadLink = downloadsAnchor.innerHTML += (
+            '<li>'
+            + 'Download: <a href="#" download="' + data.name + '" id="recvd-' + self.numReceivedFiles + '">' + data.name + '</a>'
+            + " (" + data.size + " bytes)"
+            + '</li>'
+        )
+        self.receivedFiles[data.name] = data;
+        self.receivedFiles[data.name].id = self.numReceivedFiles;
+        self.receivedFiles[data.name].anchor = document.getElementById('recvd-' + self.numReceivedFiles);
+        self.incomingFile = self.receivedFiles[data.name]
+    }
+
+    self.receiveFile = function(event) {
+        self.startTime = self.startTime || (new Date).getTime();
+        self.receiveBuffer = self.receiveBuffer || [];
+        self.receivedSize = self.receivedSize || 0;
+        self.bitrateMax = self.bitrateMax || 0;
+
+        self.receiveBuffer.push(event.data);
+        self.receivedSize += event.data.byteLength;
+
+        receiveProgress.value = self.receivedSize;
+
+        // we are assuming that our signaling protocol told
+        // about the expected file size (and name, hash, etc).
+        var received = new window.Blob(self.receiveBuffer);
+        self.receiveBuffer = [];
+
+        var url = URL.createObjectURL(received);
+        self.incomingFile.anchor.href = url;
+
+        var bitrate = Math.round(self.receivedSize * 8 /
+            ((new Date()).getTime() - self.startTime));
+
+        if (bitrate > self.bitrateMax) {
+            self.bitrateMax = bitrate;
+        }
+
+        bitrateDiv.innerHTML = '<strong>Bitrate:</strong> ' +
+            bitrate + ' kbits/sec (max: ' + self.bitrateMax + ' kbits/sec)';
+
+        if (self.receivedSize >= self.incomingFile.size) {
+            self.writeToChatLog(
+                "Receiving " + self.incomingFile.name + " finished! Download it below.",
+                "text-success sent insecure",
+                false
+            );
+            self.activeChannel.send(JSON.stringify({
+                message: 'Received ' + self.incomingFile.name + " succesfully.",
+            }));
+        } else {
+            self.incomingFile.anchor.disabled = true;
+        }
+    }
+
+    self.sendFile = function(files) {
+        var file = [].concat.apply([], files).slice(-1)[0];
+        console.log('File is ' + [file.name, file.size, file.type,
+            file.lastModifiedDate
+        ].join(' '));
+        self.activeChannel.send(JSON.stringify({
+            file: true,
+            name: file.name,
+            size: file.size,
+        }));
+        self.writeToChatLog("Sending " + file.name, "text-success sent insecure", false);
+
+        // Handle 0 size files.
+        statusMessage.textContent = '';
+        if (file.size === 0) {
+          bitrateDiv.innerHTML = '';
+          statusMessage.textContent = 'File is empty, please select a non-empty file';
+          return;
+        }
+        sendProgress.max = file.size;
+        receiveProgress.max = file.size;
+        var chunkSize = 16384;
+        var sliceFile = function(offset) {
+          var reader = new window.FileReader();
+          reader.onload = (function() {
+            return function(e) {
+              self.activeChannel.send(e.target.result);
+              if (file.size > offset + e.target.result.byteLength) {
+                window.setTimeout(sliceFile, 0, offset + chunkSize);
+              }
+              sendProgress.value = offset + e.target.result.byteLength;
+            };
+          })(file);
+          var slice = file.slice(offset, offset + chunkSize);
+          reader.readAsArrayBuffer(slice);
+        };
+        sliceFile(0);
     }
 
 /* Utilities */
